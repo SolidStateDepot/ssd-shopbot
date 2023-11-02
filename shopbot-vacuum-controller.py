a19c4650febbbddab59e87135c15972780c34e09
Sebastian Kuzminsky <seb@highlab.com>
2023-11-01 19:40:31 -0600

    misc config changes not yet committed to git :-/

---
+++ shopbot-vacuum-controller.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+
+import hal
+import os
+import time
+
+
+def turn_on():
+    os.system('wget http://shopbot-vacuum.local/cm?cmnd=Power%20On > /dev/null 2> /dev/null') 
+
+def turn_off():
+    os.system('wget http://shopbot-vacuum.local/cm?cmnd=Power%20Off > /dev/null 2> /dev/null') 
+
+
+h = hal.component("shopbot-vacuum-controller")
+h.newpin("on", hal.HAL_BIT, hal.HAL_IN)
+h.ready()
+
+old_state = False
+
+try:
+    while 1:
+        new_state = h['on']
+        if new_state and not old_state:
+            turn_on()
+        elif old_state and not new_state:
+            turn_off()
+        old_state = new_state
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
